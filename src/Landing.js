98d6d132f1f1b1de1c8433e615fce75213e8790c
Faithfulness Alamu <vaguemail369@gmail.com>
2020-11-14 21:54:46 +0100

    Update landing page UI
     - Add intro section
     - Add footer

--- src/Landing.js
+++ src/Landing.js
@@ -2,22 +2,39 @@ import React from 'react'
 import './Landing.css'
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 
 export default function Landing() {
 	return (
-		<div id="header">
-			<div className="right">
-				<Button id="headerlogin" color="secondary">Login</Button>
-				<Button variant="contained" size="small" color="secondary">Create an Account</Button>
-			</div>
-			<div id="headercontainer">
-				<img id="appicon" src={window.location.origin + '/assets/portfolio.svg'} />
-				<div id="headercharge">
-					<h1> Showboat </h1>
-					<p> You create amazing stuff,<br/> build a portfolio page to showcase them. </p>
-					<Button variant="contained" size="large" color="secondary">Get started</Button>
+		<>
+			<section id="header">
+				<div className="right">
+					<Button id="headerlogin" color="secondary">Login</Button>
+					<Button variant="contained" size="small" color="secondary">Create an Account</Button>
 				</div>
-			</div>
-		</div>
+				<div id="headercontainer">
+					<img id="appicon" src={window.location.origin + '/assets/portfolio.svg'} />
+					<div id="headercharge">
+						<h1> Showboat </h1>
+						<p> You create amazing stuff,<br/> build a portfolio page to showcase them. </p>
+						<Button variant="contained" size="large" color="secondary">Get started</Button>
+					</div>
+				</div>
+			</section>
+		    <section id="desc">
+			</section>
+		    <section id="intro">
+				<img id="introimg" src={window.location.origin + '/assets/interview.svg'} />
+				<div id="intromessage"> 
+					<p id="charge">Get that job you've <br/> always wanted.</p>
+					<p id="explain"> Keep your portfolio up to date <br/> Share with recruiters</p>
+					<Button variant="contained" color="secondary">Create an Account</Button>
+				</div>
+			</section>
+			<footer>
+				<img src={window.location.origin + '/assets/portfolio.svg'} />
+				<p>  © 2020, <a href="https://thealamu.tech">Faithfulness Alamu</a>. All rights reserved. </p>
+			</footer>
+		</>
 	)
 }
