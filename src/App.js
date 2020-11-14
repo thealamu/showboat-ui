a2099b42934fa1504e1eac3ba16550d5a225ad24
Faithfulness Alamu <vaguemail369@gmail.com>
2020-11-14 18:45:43 +0100

    Use a css baseline

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Landing from './Landing.js'
 import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
 	const theme = createMuiTheme({
@@ -15,9 +16,12 @@ function App() {
 	})
 
   return (
-	 <ThemeProvider theme={theme}> 
-		  <Landing />
-     </ThemeProvider>
+	  <>
+		<CssBaseline />
+		<ThemeProvider theme={theme}> 
+			  <Landing />
+		</ThemeProvider>
+	  </>
   );
 }
 
