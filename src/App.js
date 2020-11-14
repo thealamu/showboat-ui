91060f0582fa7d830615cb101f86e06984f81b7c
Faithfulness Alamu <vaguemail369@gmail.com>
2020-11-14 18:39:34 +0100

    Add material design system library

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
+import Landing from './Landing.js'
+import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles'
 
 function App() {
+	const theme = createMuiTheme({
+	  palette: {
+			  primary: {
+					main: '#a5d6a7',
+			  },
+			  secondary: {
+					main: '#1976d2',
+			  },
+		},
+	})
+
   return (
-	  <h1>Hello Guest</h1>
+	 <ThemeProvider theme={theme}> 
+		  <Landing />
+     </ThemeProvider>
   );
 }
 
